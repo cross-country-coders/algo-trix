11d5eba144e5d506f04df1d9a378b29fc3ffd088
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-28 16:22:17 +0900

    change

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Grid, Icon, Button  } from 'semantic-ui-react';
+import { Header, Grid, Icon, Button } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -30,51 +30,51 @@ class Landing extends React.Component {
               <Icon name='video' size='massive' />
             </Grid.Column>
           </Grid>
-          <br/>
+          <br />
         </div>
         <div className="black">
-          <br/>
+          <br />
           <div className="black-landing-item">
-            <Header as = 'h1' inverted textAlign='center'> ACCOUNTS</Header>
+            <Header as='h1' inverted textAlign='center'> ACCOUNTS</Header>
             <Grid columns={2} textAlign='center'>
               <Grid.Column>
-                <Header as = 'h2' inverted textAlign='center'> NEW TO Algo-Trix</Header>
+                <Header as='h2' inverted textAlign='center'> NEW TO Algo-Trix</Header>
                 <p>Create an Account.</p>
                 <Icon name='user plus' size='massive' />
               </Grid.Column>
               <Grid.Column>
-                <Header as = 'h2' inverted textAlign='center'> RETURNING TO Algo-Trix</Header>
+                <Header as='h2' inverted textAlign='center'> RETURNING TO Algo-Trix</Header>
                 <p>Sign Back In.</p>
                 <Icon name='user' size='massive' />
               </Grid.Column>
             </Grid>
-            <br/>
+            <br />
           </div>
           <div className='red'>
-            <br/>
-            <Header as = 'h5' inverted textAlign='center'>CREATED FOR ICS 427</Header>
+            <br />
+            <Header as='h5' inverted textAlign='center'>CREATED FOR ICS 427</Header>
             <Grid columns={3} textAlign='center'>
-              <Grid.Column><Header as = 'h5' inverted textAlign='center'>Cross-Country-Coders Team</Header>
+              <Grid.Column><Header as='h5' inverted textAlign='center'>Cross-Country-Coders Team</Header>
                 <ul>
-                  <li> Jerome Gallego </li>
+                  <li> Jerome Gallego</li>
                   <li> Christian Jensen</li>
                   <li> Shin Saito</li>
-                  <li> Jun Miao </li>
+                  <li> Jun Miao</li>
                 </ul>
               </Grid.Column>
               <Grid.Column>
-                <Header as = 'h5' inverted textAlign='center'>Related Links</Header>
-                <Button as='a' href ='https://cross-country-coders.github.io/' color = 'github'>
-                  <Icon name ='github'/>GitHub Repository Page
+                <Header as='h5' inverted textAlign='center'>Related Links</Header>
+                <Button as='a' href='https://cross-country-coders.github.io/' color='github'>
+                  <Icon name='github' />GitHub Repository Page
                 </Button>
-                <br/>
-                <br/>
-                <Button as='a' href ='https://cross-country-coders.github.io/' color = 'blue'>
-                  <Icon name ='github'/>Cross Country Coders
+                <br />
+                <br />
+                <Button as='a' href='https://cross-country-coders.github.io/' color='blue'>
+                  <Icon name='github' />Cross Country Coders
                 </Button>
               </Grid.Column>
               <Grid.Column>
-                Created as a class project and is not associated with any actual project research group.
+                  Created as a class project and is not associated with any actual project research group.
               </Grid.Column>
             </Grid>
           </div>
