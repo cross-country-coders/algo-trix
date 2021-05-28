09e0179931120e0e8e05e59b875e0414d0f83ddc
Shun <47809066+saitoshi@users.noreply.github.com>
2021-05-28 18:05:35 +0900

    added user profile

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -5,6 +5,7 @@ import 'semantic-ui-css/semantic.css';
 import { Roles } from 'meteor/alanning:roles';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from '../components/NavBar';
+import UserProfile from '../pages/UserProfile';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
 import ListStuff from '../pages/ListStuff';
@@ -29,6 +30,7 @@ class App extends React.Component {
             <Route path="/signup" component={Signup}/>
             <Route path="/signout" component={Signout}/>
             <ProtectedRoute path="/list" component={ListStuff}/>
+            <ProtectedRoute path="/profile" component={UserProfile}/>
             <ProtectedRoute path="/add" component={AddStuff}/>
             <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
             <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
---
+++ app/imports/ui/pages/UserProfile.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Loader, Card, Image, Button, Header, Segment } from 'semantic-ui-react';
+import { UserInfos } from '../../api/userinfo/UserInfo';
+
+class UserProfile extends React.Component {
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  renderPage() {
+    return (
+      <Segment>
+        <Header as='h2' textAlign='centered'> Profile Page </Header>
+        <Card centered>
+          <Image src={this.props.profiles.userImage} />
+          <Card.Content>
+            <Card.Header textAlign='centered'>{this.props.profiles.firstName} {this.props.profiles.lastName}</Card.Header>
+          </Card.Content>
+          <Card.Content extra>
+            {this.props.profiles.email}
+            <br />
+            <Button>Click Here</Button>
+          </Card.Content>
+        </Card>
+      </Segment>
+    );
+  }
+}
+
+UserProfile.propTypes = {
+  ready: PropTypes.bool.isRequired,
+  profiles: PropTypes.object,
+  currentUser: PropTypes.string,
+  currentId: PropTypes.string,
+};
+
+export default withTracker(({ match }) => {
+  const userID = Meteor.userId();
+  const sub1 = UserInfos.subscribeUserInfo();
+  const userAccount = Meteor.users.findOne({ _id: userID });
+  const profiles = UserInfos.findOne({ owner: userAccount?.username });
+  return {
+    profiles,
+    currentUser: Meteor.user() ? Meteor.user().username : '',
+    currentId: match.params._id,
+    ready: sub1.ready(),
+  };
+})(UserProfile);
