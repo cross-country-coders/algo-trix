27640c2966ff72071a7cdef8d09312a1ed484b5f
Shun <47809066+saitoshi@users.noreply.github.com>
2021-06-12 13:27:03 +0900

    Update ProofPractice.jsx

--- app/imports/ui/components/practice/proof/ProofPractice.jsx
+++ app/imports/ui/components/practice/proof/ProofPractice.jsx
@@ -4,10 +4,10 @@ import { Image, Tab } from 'semantic-ui-react';
 class ProofPractice extends React.Component {
   render() {
     const samplePane = [
-      { menuItem: 'Sample 1', render: () => <Tab.Pane><b>Prove the following formula</b><br/>
+      { menuItem: 'Sample 1', render: () => <Tab.Pane><b>Prove the following formula</b>
         <Image src='images/sample1.png'/></Tab.Pane> },
-      { menuItem: 'Sample 2' },
-      { menuItem: 'Sample 3' },
+      { menuItem: 'Sample 2', render: () => <Tab.Pane><b>Use a direct proof to show that the sum of two odd integers is even.</b></Tab.Pane> },
+      { menuItem: 'Sample 3', render: () => <Tab.Pane><b>Show that if n is an integer and n^3 + 5 is odd, then n is even using</b></Tab.Pane> },
     ];
     return (
       <Tab panes={samplePane}/>
