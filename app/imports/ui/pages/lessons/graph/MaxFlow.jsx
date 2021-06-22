38b81ec5892d3879817ead8cd4d8d28bf24b39e4
Shun <47809066+saitoshi@users.noreply.github.com>
2021-06-22 11:36:41 +0900

    added max flow

---
+++ app/imports/ui/pages/lessons/graph/MaxFlow.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Header, Container } from 'semantic-ui-react';
+import MaxFlowNotes from '../../../components/notes/MaxFlowNotes';
+import MaxFlowPractice from '../../../components/practice/MaxFlowPractice';
+
+export default class MST extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className='red'>
+          <Header as = 'h2' textAlign='center' inverted>Maximum Flow</Header>
+          <br/>
+        </div>
+        <Container>
+          <br/>
+          <MaxFlowNotes/>
+          <Header as = 'h3' textAlign='center'>Example Problems</Header>
+          <MaxFlowPractice/>
+          <br/>
+        </Container>
+      </div>
+    );
+  }
+}
