7148c100ebcad236ceb77eca5cce2aedd28e79ed
Shun <47809066+saitoshi@users.noreply.github.com>
2021-06-22 11:57:58 +0900

    update with Max flow problems

--- app/imports/ui/components/practice/MaxFlowPractice.jsx
+++ app/imports/ui/components/practice/MaxFlowPractice.jsx
@@ -1,50 +1,21 @@
 import React from 'react';
-import { Tab, Header, Image } from 'semantic-ui-react';
+import { Tab, Image } from 'semantic-ui-react';
 
 class MaxFlowPractice extends React.Component {
   render() {
     const proofPanes = [
-      { menuItem: 'Iterated BellMan Ford', render: () => <Tab.Pane>
-        <b>Iterated BellMan Ford</b>
-        <ul>
-          <li>Cost: O(V^2 E)</li>
-          <li>dense graph: O(V^4)</li>
-          <li>works on graph with negative weights</li>
-        </ul>
+      { menuItem: 'SAMPLE 1', render: () => <Tab.Pane>
+        <b>What is the min-cut corresponding to the max-flow in this graph? </b>
+        <Image src='images/max1.png' centered size='big'/>
       </Tab.Pane> },
-      { menuItem: 'Iterated Dijkstra', render: () => <Tab.Pane>
-        <ul>
-          <li>|V| iteragtion gives O(VE lg V)</li>
-          <li>Dense Graph takes O(V^3 lg V)</li>
-          <li>Using Fibonnaci Heaps: O(V^2 lg V + VE)</li>
-          <li>Does not work with Negative Weights</li>
-        </ul>
-      </Tab.Pane> },
-      { menuItem: 'Johnson Algorithm', render: () => <Tab.Pane>
-        <Image src='images/johnson.png' centered size='medium'/>
-        <ul>
-          <li>Define h(v) = &delta;(s,v) for all v in V</li>
-          <li>&Theta;(V) to find G</li>
-          <li>O(VE) to run BellMan ford</li>
-          <li>O(V) to find h(v)</li>
-          <li>&Theta;(E) to compute w</li>
-          <li>&Theta;(V^2) to initialize D</li>
-          <li>Overall RunTime: O(V E lg V)</li>
-          <li>With Fibonnaci Heaps: O(V^2 lg V + V E)</li>
-        </ul>
-      </Tab.Pane> },
-      { menuItem: 'Floyd Warshall', render: () => <Tab.Pane>
-        <Image src='images/floyd.png' centered size='medium'/>
-        <ul>
-          <li>Works with negative weight edges</li>
-          <li>RunTime O(V^3)</li>
-        </ul>
+      { menuItem: 'SAMPLE 2', render: () => <Tab.Pane>
+        <b>Write the correct residual network for the flow network shown below?</b>
+        <Image src='images/max2.png' centered size='big'/>
       </Tab.Pane> },
     ];
     return (
       <div>
         <br/>
-        <Header as = 'h3' textAlign='center'>All Shortest Path</Header>
         <Tab panes = {proofPanes}/>
       </div>
     );
